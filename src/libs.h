d5ad19bb93a592742dca9f439aa7ec0b5144ab29
Jianxing Zhang <jxzhang@Jianxings-MacBook-Pro-2018-13.local>
2019-05-05 13:54:18 -0400

    init all project

---
+++ src/libs.h
@@ -0,0 +1,24 @@
+//
+//  Header.h
+//  xcode
+//
+//  Created by Jianxing Zhang on 2019-05-05.
+//  Copyright © 2019 Jianxing Zhang. All rights reserved.
+//
+
+#ifndef libs_h
+#define libs_h
+#include <cmath>
+#include <vector>
+#include <uWS/uWS.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "json.hpp"
+#include "grader.h"
+
+#include <eigen3/Eigen/Dense>
+//#include "Eigen-3.3/Eigen/Dense"
+
+#endif /* Header_h */
